eac7472907d986720b6db0e8e94d6f587ef13ba6
Bo Bramer <bramer.bo@gmail.com>
2025-08-23 16:12:39 -0700

    chore: updated version in sidebar

--- src/components/menus/AppSidebar.tsx
+++ src/components/menus/AppSidebar.tsx
@@ -84,7 +84,7 @@ export function AppSidebar() {
             <SidebarMenuButton asChild>
               <Link href="https://github.com/wuguishifu/mega-client" target="_blank">
                 <Code />
-                <span>Version 0.0.2</span>
+                <span>Version 0.0.3</span>
               </Link>
             </SidebarMenuButton>
           </SidebarMenuItem>
